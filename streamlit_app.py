b69226893469c461e0ec46f7cef3f055ea40ebac
rainaanmol <140179650+rainaanmol@users.noreply.github.com>
2023-07-21 20:56:09 +0530

    Update streamlit_app.py
    added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
+streamlit.header('Breakfast menu')
+streamlit.text('omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
