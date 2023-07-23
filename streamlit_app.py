9670d7c68f814b70641948f4edfb9f58614bbfe4
rainaanmol <140179650+rainaanmol@users.noreply.github.com>
2023-07-24 00:39:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,8 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index("Fruit")
+##my_fruit_list = my_fruit_list.set_index("Fruit")
+streamlit.dataframe(my_fruit_list)
 
 #lets pick a fruit
 ###streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
@@ -20,6 +21,10 @@ my_fruit_list = my_fruit_list.set_index("Fruit")
 
 #display on the page
 ##streamlit.dataframe(fruits_show)
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
 
 
 
